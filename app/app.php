93ac560d1b4170aa907bc1678c594377efd07378
Anton Titov <wolfy.jd@gmail.com>
2021-02-09 21:26:16 +0300

    Added docker-compose app version (#3)

--- app/app.php
+++ app/app.php
@@ -20,7 +20,12 @@ require __DIR__ . '/vendor/autoload.php';
 // finds all available workflows, activity types and commands in a given directory
 $declarations = DeclarationLocator::create(__DIR__ . '/src/');
 
-$workflowClient = WorkflowClient::create(ServiceClient::create('localhost:7233'));
+$host = getenv('TEMPORAL_CLI_ADDRESS');
+if (empty($host)) {
+    $host = 'localhost:7233';
+}
+
+$workflowClient = WorkflowClient::create(ServiceClient::create($host));
 
 $app = new Application('Temporal PHP-SDK Samples');
 
--- app/src/AsyncActivityCompletion/README.md
+++ app/src/AsyncActivityCompletion/README.md
@@ -7,3 +7,11 @@ From the root of the project, run the following command to start the Workflow:
 ```bash
 php ./app/app.php user-activity:start
 ```
+
+To complete activity execution:
+
+```bash
+php ./app/app.php user-activity:complete {TOKEN} {MESSAGE}
+```
+
+> Token can be found in application log. 
