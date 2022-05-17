5a6c3b9d8d4505c0cee9699b00315cad8746629a
sergeyzhuk <seregazhuk88@gmail.com>
2022-05-17 09:15:34 +0300

    Local activity example

---
+++ app/src/LocalActivity/GreetingWorkflowInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file is part of Temporal package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Temporal\Samples\LocalActivity;
+
+// @@@SNIPSTART php-hello-workflow-interface
+use Temporal\Workflow\WorkflowInterface;
+use Temporal\Workflow\WorkflowMethod;
+
+#[WorkflowInterface]
+interface GreetingWorkflowInterface
+{
+    /**
+     * @param string $name
+     * @return string
+     */
+    #[WorkflowMethod(name: "SimpleActivity.greet")]
+    public function greet(
+        string $name
+    );
+}
+// @@@SNIPEND
