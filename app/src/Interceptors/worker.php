c28c7f48b1276d361edcf087b2ef339f0b633dca
roxblnfk <roxblnfk@ya.ru>
2024-11-18 18:25:01 +0400

    Add Workflow Inbound Calls Logger

--- app/src/Interceptors/worker.php
+++ app/src/Interceptors/worker.php
@@ -13,17 +13,21 @@ use Temporal\Interceptor\SimplePipelineProvider;
 use Temporal\Samples\Interceptors\Activity\Sleeper;
 use Temporal\Samples\Interceptors\Interceptor\ActivityAttributesInterceptor;
 use Temporal\Samples\Interceptors\Interceptor\RequestLoggerInterceptor;
+use Temporal\Samples\Interceptors\Interceptor\LoggerWorkflowInboundCallsInterceptor;
 use Temporal\Samples\Interceptors\Workflow\TestActivityAttributesInterceptor;
+use Temporal\SampleUtils\Logger;
 use Temporal\WorkerFactory;
 
 ini_set('display_errors', 'stderr');
 include "../../vendor/autoload.php";
 
 $factory = WorkerFactory::create();
+$logger = new Logger();
 
 $worker = $factory->newWorker(taskQueue: 'interceptors', interceptorProvider: new SimplePipelineProvider([
-    new RequestLoggerInterceptor(new \Temporal\SampleUtils\Logger()),
-    new ActivityAttributesInterceptor()
+    new RequestLoggerInterceptor($logger),
+    new ActivityAttributesInterceptor(),
+    new LoggerWorkflowInboundCallsInterceptor($logger),
 ]))
     ->registerWorkflowTypes(TestActivityAttributesInterceptor::class)
     ->registerActivityImplementations(new Sleeper());
